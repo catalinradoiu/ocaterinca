788e4dfa77697df209b99e528ffa681b7c7e1bf5
Paul Boldijar <boldijar.paul@gmail.com>
2020-03-28 16:44:51 +0200

    Added resize algorithm

--- app/src/main/java/com/ocaterinca/ocaterinca/feature/AvatarCardViewModel.kt
+++ app/src/main/java/com/ocaterinca/ocaterinca/feature/AvatarCardViewModel.kt
@@ -1,15 +1,36 @@
 package com.ocaterinca.ocaterinca.feature
 
+import android.util.Log
 import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import androidx.databinding.ObservableFloat
+import com.dtl.kaloric.refactoring.utils.ImageUtils
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import java.io.File
 
 class AvatarCardViewModel {
 
     val imagePath = ObservableField<String>()
     fun pickedImage(file: File) {
-        imagePath.set(file.absolutePath)
+        resizeImage(file.absolutePath)
+    }
+
+    private fun resizeImage(absolutePath: String) {
+        doAsync {
+            try {
+                val resizedFile = ImageUtils.resizeImageKeepAspectRatio(absolutePath, 200, 200)
+                uiThread {
+                    imageWasResized(resizedFile)
+                }
+            } catch (e: Exception) {
+                Log.e("AvatarCardModel", "Eroare", e)
+            }
+        }
+    }
+
+    private fun imageWasResized(resizedFile: File) {
+        imagePath.set(resizedFile.absolutePath)
         nextEnabled.set(true)
     }
 
