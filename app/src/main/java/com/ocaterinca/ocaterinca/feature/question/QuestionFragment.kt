43959f2368a807847041c9d0972073edfe783e5a
catalinradoiu <radoiu_catalin1996@yahoo.com>
2020-03-28 17:22:11 +0200

    Add players list with recycler adapter to QuestionFragment

--- app/src/main/java/com/ocaterinca/ocaterinca/feature/question/QuestionFragment.kt
+++ app/src/main/java/com/ocaterinca/ocaterinca/feature/question/QuestionFragment.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ocaterinca.ocaterinca.QuestionFragmentBinding
 import com.ocaterinca.ocaterinca.R
+import com.ocaterinca.ocaterinca.feature.question.player.PlayersAdapter
 
 class QuestionFragment : Fragment() {
 
     private lateinit var binding: QuestionFragmentBinding
+    private val playersAdapter = PlayersAdapter()
+    private val viewModel = QuestionViewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_question, container, false)
@@ -20,6 +26,14 @@ class QuestionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.playersList.apply {
+            adapter = playersAdapter
+            layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        }
+
+        viewModel.playersList.observe(viewLifecycleOwner, Observer {
+            playersAdapter.submitList(it)
+        })
     }
 
 }
--- app/src/main/java/com/ocaterinca/ocaterinca/feature/question/QuestionViewModel.kt
+++ app/src/main/java/com/ocaterinca/ocaterinca/feature/question/QuestionViewModel.kt
@@ -3,6 +3,7 @@ package com.ocaterinca.ocaterinca.feature.question
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ocaterinca.ocaterinca.core.model.Player
 
 class QuestionViewModel : ViewModel() {
 
@@ -14,5 +15,18 @@ class QuestionViewModel : ViewModel() {
 
     private val _secondPlayerAvatarUrl = MutableLiveData<String>()
     val secondPlayerAvatarUrl: LiveData<String> = _secondPlayerAvatarUrl
+
+    private val _playersList = MutableLiveData<List<Player>>().apply {
+        value = PLAYERS_MOCK_LIST
+    }
+    val playersList: LiveData<List<Player>> = _playersList
+
+    companion object {
+        private val PLAYERS_MOCK_LIST = listOf(
+            Player("1", "https://www.w3schools.com/w3images/avatar2.png"),
+            Player("1", "https://www.w3schools.com/w3images/avatar2.png"),
+            Player("1", "https://www.w3schools.com/w3images/avatar2.png")
+        )
+    }
 
 }
