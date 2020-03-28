5ca013c5b6d60db8f3c1c0361927eb99fae40913
catalinradoiu <radoiu_catalin1996@yahoo.com>
2020-03-28 16:51:04 +0200

    Add live data with question details in QuestionViewModel

--- app/src/main/java/com/ocaterinca/ocaterinca/feature/question/QuestionViewModel.kt
+++ app/src/main/java/com/ocaterinca/ocaterinca/feature/question/QuestionViewModel.kt
@@ -1,8 +1,18 @@
 package com.ocaterinca.ocaterinca.feature.question
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class QuestionViewModel : ViewModel() {
 
+    private val _questionText = MutableLiveData<String>()
+    val questionText: LiveData<String> = _questionText
+
+    private val _firstPlayerAvatarUrl = MutableLiveData<String>()
+    val firstPlayerAvatarUrl : LiveData<String> = _firstPlayerAvatarUrl
+
+    private val _secondPlayerAvatarUrl = MutableLiveData<String>()
+    val secondPlayerAvatarUrl: LiveData<String> = _secondPlayerAvatarUrl
 
 }
